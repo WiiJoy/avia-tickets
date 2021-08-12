3dfb96c16b3a4c7fb8e59bd3198ab8e8925de613
Vitaliy (work) <wiijoy@bk.ru>
2021-08-12 09:04:29 +0300

    add api service for countries and cities

---
+++ src/js/config/apiConfig.js
@@ -0,0 +1,7 @@
+// конфиг апи, к которому идет обращение (сервер)
+
+const config = {
+    url: 'https://aviasales-api.herokuapp.com'
+};
+
+export default config;
---
+++ src/js/services/apiService.js
@@ -0,0 +1,41 @@
+// класс с набором методов для взаимодействия с сервером
+
+import axios from 'axios';
+import config from '../config/apiConfig';
+
+/** эндпоинты
+ * /countries - возвращает массив поддерживаемых стран
+ * /cities - возврашает массив городов
+ * /prices/cheap - возвращает массив доступных рейсов
+ */
+class Api {
+    constructor(config) {
+        this.url = config.url;
+    }
+
+    async countries() {
+        try {
+            const response = await axios.get(`${this.url}/countries`);
+            return response.data;
+        } catch(err) {
+            console.log(err);
+            console.log('error with countries list');
+            return Promise.reject(err);
+        }
+    }
+    async cities() {
+        try {
+            const response = await axios.get(`${this.url}/cities`);
+            return response.data;
+        } catch(err) {
+            console.log(err);
+            console.log('error with cities list');
+            return Promise.reject(err);
+        }
+    }
+    async prices(params) {}
+}
+
+const api = new Api(config);
+
+export default api;

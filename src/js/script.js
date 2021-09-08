4e2886cefc8a3a0c3372cb507b0ca52f4cb7049e
Vitaliy (work) <wiijoy@bk.ru>
2021-09-08 14:11:03 +0300

    Add autocomplete form

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,25 @@ import '../css/style.css';
 
 import locations from './store/locations';
 
-locations.init().then(res => {
-    console.log(res);
-    console.log(locations);
-    console.log(locations.getCitiesByCountryCode('PE'));
+import formUI from './views/form';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+
+    // Events
+
+
+
+    // Handlers
+    async function initApp() {
+        await locations.init();
+        formUI.setAutocompleteData(locations.shortCitiesList);
+    }
+
 });
+
+// locations.init().then(res => {
+//     console.log(res);
+//     console.log(locations);
+//     console.log(locations.getCitiesByCountryCode('PE'));
+// });
--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -5,6 +5,7 @@ class Locations {
         this.api = api;
         this.countries = null;
         this.cities = null;
+        this.shortCitiesList = null;
     }
 
     async init() {
@@ -16,10 +17,43 @@ class Locations {
         console.log(response);
 
         const [countries, cities] = response;
-        this.countries = countries;
-        this.cities = cities;
+        this.countries = this.serializeCountries(countries);
+        this.cities = this.serializeCities(cities);
+        this.shortCitiesList = this.createShortCitiesList(this.cities);
 
         return response;
+    }
+
+    createShortCitiesList(cities) {
+        // { 'City, Country': null }
+        // Object.entries => [key, value]
+        return Object.entries(cities).reduce((acc, [key]) => {
+            acc[key] = null;
+            return acc;
+        }, {});
+    }
+
+    serializeCountries(countries) {
+        // { 'Country code': { ... } }
+        return countries.reduce((acc, country) => {
+            acc[country.code] = country;
+            return acc;
+        }, {});
+    }
+
+    serializeCities(cities) {
+        // { 'City name, Country name': { ... } }
+        return cities.reduce((acc, city) => {
+            const countryName = this.getCountryNameByCode(city.country_code);
+            const cityName = city.name || city.name_translation.en;
+            const key = `${cityName},${countryName}`;
+            acc[key] = city;
+            return acc;
+        }, {});
+    }
+
+    getCountryNameByCode(code) {
+        return this.countries[code].name;
     }
 
     getCitiesByCountryCode(code) {
